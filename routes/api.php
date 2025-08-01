5a1013e3dff377bceb744fbbd77aa99e37a28789
paraiso3d <normanparaiso.abm12@gmail.com>
2025-08-02 00:15:17 +0800

    z

--- routes/api.php
+++ routes/api.php
@@ -98,9 +98,8 @@ Route::post('approveadmission/{id}', [AdmissionsController::class, 'approveAdmis
 // Route::post('verifyaccount', [AccountsController::class, 'verifyAccount']);
 
 // // School Campus Management
-Route::middleware('auth:sanctum')->group(function (){
- 
-Route::get('getcampuses', [SchoolCampusController::class, 'getCampuses']);   
+Route::get('getcampuses', [SchoolCampusController::class, 'getCampuses']);
+Route::middleware('auth:sanctum')->group(function (){ 
 Route::post('addcampus', [SchoolCampusController::class, 'addCampus']);
 Route::post('updatecampus/{id}', [SchoolCampusController::class, 'updateCampus']);
  });
